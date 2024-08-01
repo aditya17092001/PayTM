b7e08d9553e1a913763e847b7d5fadc3a769281b
aditya17092001 <adityaudata@gmail.com>
2024-08-01 20:48:17 +0530

    init

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,7 @@
+// import { PrismaClient } from "@repo/db/client";
+// const client = new PrismaClient();
+export default function Home() {
+  return (
+   <div className=" font-extrabold bg-slate-500">Hai</div>
+  );
+}
